3c34fc030ade8d329e8fbd60fe6b41824660d52d
zeirwayne <zeirwayne@github.com>
2019-06-17 18:38:44 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header";
+import Register from "./components/Register";
 import Categories from "./components/Categories";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
+      <Register />
       <Header />
       <Categories />
       <Footer />
